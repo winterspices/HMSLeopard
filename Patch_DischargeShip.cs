0c27108d9c00ae8327d2a8fe03fe5481204e04e3
BuildTools <unconfigured@null.spigotmc.org>
2026-04-18 17:10:51 +1000

    Patched small bugs

---
+++ Patch_DischargeShip.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Leopard
+{
+    [HarmonyPatch(typeof(Shipyard), "DischargeShip")]
+    public class Patch_DischargeShip
+    {
+        public static void Prefix(Shipyard __instance, GameObject ___currentShip)
+        {
+            GameObject obj = GameObject.Find("shipyard Al'Ankh/ship release pos");
+
+            if (___currentShip.name == "BOAT LEOPARD (207)(Clone)")
+            {
+                obj.transform.localPosition = new Vector3(-46.8f, -9.85f, -1.1f);
+            } else
+            {
+                obj.transform.localPosition = new Vector3(13.9f, -9.85f, -0.8f);
+            }
+        }
+    }
+}
