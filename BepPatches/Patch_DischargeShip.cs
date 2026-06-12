d452d5c9b4fc828fd26c08104ae9b5032fcd0509
BuildTools <unconfigured@null.spigotmc.org>
2026-06-13 09:34:55 +1000

    Updated for v1.3.1

--- BepPatches/Patch_DischargeShip.cs
+++ BepPatches/Patch_DischargeShip.cs
@@ -11,26 +11,44 @@ namespace Leopard.BepPatches
         {
             try
             {
-                GameObject obj = GameObject.Find("shipyard Al'Ankh/ship release pos");
+
                 Shipyard shipyard = GameState.currentShipyard;
 
+                // GRC
                 if (shipyard.transform.name == "shipyard Al'Ankh")
                 {
+                    GameObject releasePos = GameObject.Find("shipyard Al'Ankh/ship release pos");
+
                     if (___currentShip.name == "BOAT LEOPARD (207)(Clone)")
                     {
-                        obj.transform.localPosition = new Vector3(-46.8f, -9.85f, -1.1f);
+                        releasePos.transform.localPosition = new Vector3(-46.8f, -9.85f, -1.1f);
                     }
                     else
                     {
-                        obj.transform.localPosition = new Vector3(13.9f, -9.85f, -0.8f);
+                        releasePos.transform.localPosition = new Vector3(13.9f, -9.85f, -0.8f);
                     }
                 }
-            } catch (Exception)
+                // Aestrin
+                else if (shipyard.transform.name == "shipyard Aestrin")
+                {
+                    GameObject releasePos = GameObject.Find("shipyard Aestrin/ship release pos");
+
+                    if (___currentShip.name == "BOAT LEOPARD (207)(Clone)")
+                    {
+                        releasePos.transform.localPosition = new Vector3(-26.8f, -12.4f, -8.1f);
+                    }
+                    else
+                    {
+                        releasePos.transform.localPosition = new Vector3(-6.6f, -12.4f, 18.4f);
+                    }
+                }
+            }
+            catch (Exception)
             {
                 // probably not al ankh
                 return;
             }
-            
+
         }
     }
 }
